df626cd51f2b1546e859d79fe1f7afc3a8c8818d
S4nchesFernando <fernando.sanches@criamosmomentum.com>
2026-01-30 11:44:17 -0300

    agora vai gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,27 +44,32 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR" className="bg-[#06070d]">
+      {/* GTM - HEAD */}
       <Script
         id="gtm-head"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-WVGQJFGS');
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WVGQJFGS');
           `,
         }}
       />
       <body className={`${inter.className} bg-[#06070d] text-slate-100 antialiased`}>
+        {/* GTM - BODY */}
         <noscript>
-          <iframe 
+          <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-WVGQJFGS"
-            height="0" 
-            width="0" 
-            style="display:none;visibility:hidden">
-          </iframe>
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
         </noscript>
         {children}
         <ConciergeWidget />
